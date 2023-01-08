cee9696d65834e576e088cbd26ccd2544793a781
lingleiming <120040573+lingleiming@users.noreply.github.com>
2023-01-08 18:24:21 +0800

    Update jianying.js

--- jianying.js
+++ jianying.js
@@ -3,6 +3,6 @@
 #剪映
 #*******************/
 
-^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Scripts/main/JY.js
+^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/lingleiming/QXgz/main/jianying.js
 
 hostname = *faceu*
