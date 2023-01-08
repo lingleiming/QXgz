d0f029713838e763d4d3d903818a27085f329e88
lingleiming <120040573+lingleiming@users.noreply.github.com>
2023-01-08 18:21:47 +0800

    Create new file

---
+++ jianying.js
@@ -0,0 +1,8 @@
+#/*****************
+#脚本作者：伟人
+#剪映
+#*******************/
+
+^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Scripts/main/JY.js
+
+hostname = *faceu*
